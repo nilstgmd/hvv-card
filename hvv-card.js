1820894552acc63635234ab898542b0458218c00
Jörn Stampehl <joern@stampehl.de>
2022-01-07 20:59:02 +0100

    Removed automatic formatting

--- hvv-card.js
+++ hvv-card.js
@@ -6,19 +6,19 @@ const css = LitElement.prototype.css;
 
 function hasConfigOrEntityChanged(element, changedProps) {
     if (changedProps.has("_config")) {
-        return true;
+      return true;
     }
 
     const oldHass = changedProps.get("hass");
     if (oldHass) {
-        return (
-            oldHass.states[element._config.entity] !==
-            element.hass.states[element._config.entity]
-        );
+      return (
+        oldHass.states[element._config.entity] !==
+          element.hass.states[element._config.entity]
+      );
     }
 
     return true;
-}
+  }
 
 class HvvCard extends LitElement {
     static get properties() {
@@ -48,19 +48,19 @@ class HvvCard extends LitElement {
 
         if (!stateObj) {
             return html `
-                <style>
-                    .not-found {
-                        flex: 1;
-                        background-color: yellow;
-                        padding: 8px;
-                    }
-                </style>
-                <ha-card>
-                    <div class="not-found">
-                        Entity not available: ${this._config.entity}
-                    </div>
-                </ha-card>
-            `;
+          <style>
+            .not-found {
+              flex: 1;
+              background-color: yellow;
+              padding: 8px;
+            }
+          </style>
+          <ha-card>
+            <div class="not-found">
+              Entity not available: ${this._config.entity}
+            </div>
+          </ha-card>
+        `;
         }
 
         const today = new Date();
@@ -68,49 +68,49 @@ class HvvCard extends LitElement {
         var count = 0;
 
         return html `
-            <ha-card>
-                ${
-                        stateObj.attributes['friendly_name']
-                                ? html`
-                                    <h1 class="card-header">${stateObj.attributes['friendly_name']}</h1>
-                                `
-                                : ""
-                }
-                <div>
-                    <table>
-                        ${stateObj.attributes['next'].map(attr => {
-                            const direction = attr['direction'];
-                            const line = attr['line'];
-                            const type = attr['type'];
-                            const delay = attr['delay'];
-                            const departure = new Date(attr["departure"]);
-                            const diffMs = (departure - today);
-                            const departureInMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+        <ha-card>
+          ${
+            stateObj.attributes['friendly_name']
+              ? html`
+                <h1 class="card-header">${stateObj.attributes['friendly_name']}</h1>
+                `
+              : ""
+          }
+          <div>
+            <table>
+                ${stateObj.attributes['next'].map(attr => {
+                    const direction = attr['direction'];
+                    const line = attr['line'];
+                    const type = attr['type'];
+                    const delay = attr['delay'];
+                    const departure = new Date(attr["departure"]);
+                    const diffMs = (departure - today);
+                    const departureInMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
 
-                            count++;
+                    count++;
 
-                            return count <= max
-                                    ? html`
-                                        <tr>
-                                            <td class="narrow" style="text-align:center;"><span class="line ${type} ${line}">${line}</span></td>
-                                            <td class="expand">${direction}</td>
-                                            <td class="narrow" style="text-align:right;">
-                                                ${this._config.show_time ?
-                                                        departure.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) :
-                                                        html `${departureInMins} min`
-                                                }
-                                                ${delay > 0 ?
-                                                        html`<span class="delay">+${delay}</span> min` :
-                                                        html``}
-                                            </td>
-                                        </tr>
-                                    `
-                                    : html ``;
-                        })}
-                    </table>
-                </div>
-            </ha-card>
-        `;
+                    return count <= max
+                    ? html`
+                        <tr>
+                            <td class="narrow" style="text-align:center;"><span class="line ${type} ${line}">${line}</span></td>
+                            <td class="expand">${direction}</td>
+                            <td class="narrow" style="text-align:right;">
+                                ${this._config.show_time ? 
+                                    departure.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) :
+                                    html `${departureInMins} min`
+                                }
+                                ${delay > 0 ?
+                                    html`<span class="delay">+${delay}</span> min` :
+                                    html``}
+                            </td>
+                        </tr>
+                        `
+                    : html ``;
+                })}
+            </table>
+          </div>
+        </ha-card>
+      `;
     }
 
     getCardSize() {
@@ -119,110 +119,110 @@ class HvvCard extends LitElement {
 
     static get styles() {
         return css`
-          table {
+        table {
             width: 100%;
             padding: 6px 14px;
-          }
+        }
 
-          td {
+        td {
             padding: 3px 0px;
-          }
+        }
 
-          td.narrow {
+        td.narrow {
             white-space: nowrap;
-          }
+        }
 
-          td.expand {
+        td.expand {
             width: 95%
-          }
+        }
 
-          span.line {
+        span.line {
             font-weight: bold;
             font-size: 0.9em;
             padding: 3px 8px 2px 8px;
             color: #ffffff;
             background-color: #888888;
             margin-right: 0.7em;
-          }
+        }
 
-          span.delay {
-            color: #e2001a;
-          }
+        span.delay {
+             color: #e2001a;
+        }
 
-          span.S, span.A{
+        span.S, span.A{
             background-color: #009252;
             border-radius: 999px;
-          }
+        }
 
-          span.U {
+        span.U {
             border-radius: 0px;
-          }
+        }
 
-          span.Bus, span.XpressBus, span.Schnellbus, span.NachtBus {
+        span.Bus, span.XpressBus, span.Schnellbus, span.NachtBus {
             background-color: #e2001a;
             clip-path: polygon(20% 0, 80% 0, 100% 50%, 80% 100%, 20% 100%, 0 50%);
             width: 48px;
             margin-left: 0;
-          }
+        }
 
-          span.XpressBus {
+        span.XpressBus {
             background-color: #1a962b;
-          }
+        }
 
-          span.NachtBus {
+        span.NachtBus {
             background-color: #000000;
-          }
+        }
 
-          span.Schiff {
+        span.Schiff {
             background-color: #009dd1;
             clip-path: polygon(0 0, 100% 0, 90% 100%, 10% 100%);
-          }
+        }
 
-          span.ICE, span.RE, span.EC, span.IC, span.RB, span.R {
+        span.ICE, span.RE, span.EC, span.IC, span.RB, span.R {
             background-color: transparent;
             color: #000;
-          }
+        }
 
-          span.U1 {
+        span.U1 {
             background-color: #1c6ab3;
-          }
+        }
 
-          span.U2 {
+        span.U2 {
             background-color: #e2021b;
-          }
+        }
 
-          span.U3 {
+        span.U3 {
             background-color: #fddd00;
-          }
+        }
 
-          span.U4 {
+        span.U4 {
             background-color: #0098a1;
-          }
+        }
 
-          span.S1 {
+        span.S1 {
             background-color: #31962b;
-          }
+        }
 
-          span.S2 {
+        span.S2 {
             background-color: #b51143;
-          }
+        }
 
-          span.S3 {
+        span.S3 {
             background-color: #622181;
-          }
+        }
 
-          span.S11 {
+        span.S11 {
             background-color: #31962b;
-          }
+        }
 
-          span.S21 {
+        span.S21 {
             background-color: #b51143;
-          }
+        }
 
-          span.S31 {
+        span.S31 {
             background-color: #622181;
-          }
-        `;
+        }
+      `;
     }
 }
 customElements.define("hvv-card", HvvCard);
