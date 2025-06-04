bb89fc3c00778785e27fad6b3acea4b0b0d90a9e
Nils Meder <nilstgmd@users.noreply.github.com>
2025-06-04 13:18:54 +0200

    fix unavailable html

--- hvv-card.js
+++ hvv-card.js
@@ -92,6 +92,7 @@ class HvvCard extends LitElement {
                                 `
                             : ""
                             }
+                            </div>
                         `;
                     }
 
