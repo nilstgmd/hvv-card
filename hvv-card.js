4a337a5ca7c2e60378c36ee9b940b9cff8425450
Phillipp <jaeger.phillipp@web.de>
2025-11-28 13:46:26 +0100

    improve U3 readability

--- hvv-card.js
+++ hvv-card.js
@@ -246,6 +246,7 @@ class HvvCard extends LitElement {
 
         span.U3 {
             background-color: #fddd00;
+            text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
         }
 
         span.U4 {
