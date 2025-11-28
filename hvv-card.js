ce6a91bb2bf21503844685c8264dcc0f53fdac54
Phillipp <jaeger.phillipp@web.de>
2025-11-28 14:14:07 +0100

    make U3 outline slighly thinner

--- hvv-card.js
+++ hvv-card.js
@@ -247,7 +247,7 @@ class HvvCard extends LitElement {
 
         span.U3 {
             background-color: #fddd00;
-            -webkit-text-stroke: 1.25px #000;
+            -webkit-text-stroke: 1.1px #000;
             font-size: 1.1em;
         }
 
