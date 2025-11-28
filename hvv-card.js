04f2d6c8ef554e7122070d9a37682802c5d13240
Phillipp <jaeger.phillipp@web.de>
2025-11-28 15:54:38 +0100

    another try at making the U3 more readable

--- hvv-card.js
+++ hvv-card.js
@@ -247,7 +247,7 @@ class HvvCard extends LitElement {
 
         span.U3 {
             background-color: #fddd00;
-            -webkit-text-stroke: 0.8px #000;
+            -webkit-text-stroke: 0.5px #000;
             font-size: 1.2em;
         }
 
